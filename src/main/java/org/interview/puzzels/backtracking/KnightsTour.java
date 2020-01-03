f5b011f7023822942a5d4ff174dae09f6c1e6eef
Anthony Graffeo <graffeoa@amazon.com>
2020-01-03 16:33:33 -0500

    Some backtracking

--- src/main/java/org/interview/puzzels/backtracking/KnightsTour.java
+++ src/main/java/org/interview/puzzels/backtracking/KnightsTour.java
@@ -3,8 +3,8 @@ package org.interview.puzzels.backtracking;
 import java.util.Arrays;
 
 public class KnightsTour {
-    static int rowMove[] = {2, 1, -1, -2, -2, -1, 1, 2};
-    static int colMove[] = {1, 2, 2, 1, -1, -2, -2, -1};
+    static int rowMove[] = {2, 1, -1, -2, -2, -1,  1, 2};
+    static int colMove[] = {1, 2,  2,  1, -1, -2, -2, -1};
 
     boolean tourTheBoard(int[][] board, int N) {
         Arrays.stream(board).forEach(cols -> Arrays.fill(cols, -1));
@@ -12,7 +12,7 @@ public class KnightsTour {
         return tour(board, 0, 0, 1, N);
     }
 
-    boolean tour(int[][] board, int row, int col, int count, int N) {
+    boolean tour(final int[][] board, final int row, final int col, final int count, final int N) {
         if(count == (N*N)) {
             return true;
         }
@@ -20,7 +20,6 @@ public class KnightsTour {
         for (int i = 0; i < 8; i++) {
             int nextRow = row + rowMove[i];
             int nextCol = col + colMove[i];
-
             if (nextRow >= 0 && nextRow < N && nextCol >= 0 && nextCol < N && board[nextRow][nextCol] == -1) {
                 board[nextRow][nextCol] = count;
                 if (tour(board, nextRow, nextCol, count + 1, N)) {
@@ -36,16 +35,15 @@ public class KnightsTour {
 
     void printBoard(int[][] board) {
         System.out.println();
-        Arrays.stream(board)
-                .forEach(cols -> { Arrays.stream(cols)
-                                    .forEach(i -> System.out.printf("%d, ", i));
-                                    System.out.println();
-                });
+        Arrays.stream(board).forEach(cols -> {
+            Arrays.stream(cols).forEach(i -> System.out.printf("%d, ", i));
+            System.out.println();
+        });
     }
 
     public static void main(String[] args) {
         KnightsTour o = new KnightsTour();
-        final int N = 7;
+        final int N = 8;
         int[][] board = new int[N][N];
 
         System.out.printf("%s solvable for %d x %d board!!!!!!!!!!\n",
