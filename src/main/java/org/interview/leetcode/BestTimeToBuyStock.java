d3da71855b434dfe0b43b9bcce187fc1d41ba08e
Anthony Graffeo <agraffeo@gmail.com>
2020-01-29 08:09:13 -0500

    best time

---
+++ src/main/java/org/interview/leetcode/BestTimeToBuyStock.java
@@ -0,0 +1,55 @@
+package org.interview.leetcode;
+
+import java.util.Arrays;
+
+/*
+
+       1  2  3  4  5  6  7
+    3, 2, 6, 5, 0, 3
+ 0  0  0  0  0  0  0  0  0
+ 1  0
+ 2  0
+ 3  0 
+ */
+public class BestTimeToBuyStock {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if(n == 0 || k == 0) {
+            return 0;
+        }
+        int[][] transaction = new int[2][prices.length];
+//        Arrays.fill(transaction[0], 0);
+//        Arrays.fill(transaction[1], 0);
+
+        for(int col = 0; col < prices.length; col++) {
+            transaction[0][col] = 0;
+        }
+
+        int current = 0;
+        int prev = 0;
+        for(int row = 0; row < Math.min(k, n/2); row++) {
+            current = row % 2;
+            prev = (row + 1) % 2;
+            int max = Integer.MIN_VALUE;
+            for (int col = 1; col < n; col++) {
+                if(col == 0) {
+                    transaction[current][col] = 0;
+                }
+                max = Math.max(max, transaction[prev][col - 1] - prices[col -1]);
+                transaction[current][col] = Math.max(transaction[current][col-1], prices[col] + max);
+            }
+        }
+
+        return transaction[current][n - 1];
+    }
+
+    static public void main(String[] args) {
+        BestTimeToBuyStock o = new BestTimeToBuyStock();
+        int[] prices = {1,2,4,2,5,7,2,4,9,0};
+        int k = 4;
+        System.out.println(o.maxProfit(k, prices));
+        prices = new int[]{2,4,1};
+        k = 2;
+        System.out.println(o.maxProfit(k, prices));
+    }
+}
